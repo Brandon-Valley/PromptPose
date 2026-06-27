8ad49bd09ed403e3d4f1b66338214c894957e10b
Brandon-Valley <bvalley111@gmail.com>
2026-06-27 13:28:29 -0500

    Delete .github directory

--- .github/make_index.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-p = Path('app/app.gz.b64.00.txt')
-q = Path('app/app.gz.b64.01.txt')
-text = p.read_text() + q.read_text()
-b64 = __import__('base64')
-gz = __import__('gzip')
-data = b64.b64decode(text)
-html = gz.decompress(data).decode('utf-8')
-Path('index.html').write_text(html, encoding='utf-8')
